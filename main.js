47571d559a20db86dd8158582c42c0e039d4deb5
Ahnaf Shahriar <shahriarahnaf007@gmail.com>
2021-08-30 23:37:46 -0400

    axios support added

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+const { default: axios } = require('axios')
 const { app, BrowserWindow } = require('electron')
+//use axios.all to get multiple data points if that is possible 
+//or use axios.get to use one single response point for the program
 
+axios.get();
 function createWindow () {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 1000
     })
   
     win.loadFile('index.html')
