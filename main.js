824d6e34386bf12022d7cdfd58f7b13526c7aaa6
Ahnaf Shahriar <shahriarahnaf007@gmail.com>
2021-09-04 19:15:51 -0400

    better structure and using the differentiation of main and logic js in different files

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-const { default: axios } = require('axios')
-
-import yahooFinance from 'yahoo-finance2';
-
 const { app, BrowserWindow } = require('electron')
-//use axios.all to get multiple data points if that is possible 
-//or use axios.get to use one single response point for the program
-
 
 //making the window
 function createWindow () {
